984c3644e89d56fa2b4db0cc44b18fe814a17fc8
GabrielaGP2023 <cursos@gabrielagomezpuente.es>
2024-10-07 13:58:01 +0200

    final de clase 7 de octubre

---
+++ ejercicios07Oct/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { DetalleComponent } from './detalle/detalle.component';
+
+
+export const routes: Routes = [
+    {"path": "inicio", "component":InicioComponent},
+    {"path": "detalle/:id", "component":DetalleComponent},
+    {"path": "", redirectTo: "/inicio", pathMatch:"full"},
+    
+];
